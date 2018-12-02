f000be594f3ae124992a5fa02f871e6dd7f354c4
jacky <andrade.j1795@gmail.com>
2018-12-02 16:47:02 -0500

    first python project

---
+++ HelloWorlds/joining.py
@@ -0,0 +1,11 @@
+data = [
+    ['Boston', 'MA', '76F', '65% Precip', '0.15 in'],
+    ['San Francisco', 'CA', '62F', '20% Precip', '0.00 in'],
+    ['Washington', 'DC', '82F', '80% Precip', '0.19 in'],
+    ['Miami', 'FL', '79F', '50% Precip', '0.70 in']
+]
+joined=(','.join(row) for row in data)
+
+output= '\n'.join(joined)
+
+print(output)
